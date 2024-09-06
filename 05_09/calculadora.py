071b932d46bfe7b49749c0133f6c9c4a1270682e
eliasasa <eliasnetoneto28@gmail.com>
2024-09-05 20:26:16 -0400

    bizarro isso

--- 05_09/calculadora.py
+++ 05_09/calculadora.py
@@ -1,16 +1,15 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 
-# Inicializa a aplicação
 app = ctk.CTk()
-app.geometry('400x600')
+app.geometry('300x500')
 app.configure(fg_color='#1E1E1E')
 app.title('Calculadora')
 
-app.columnconfigure(0, weight=0)
-app.columnconfigure(1, weight=0)
-app.columnconfigure(2, weight=0)
-app.columnconfigure(3, weight=0)
+app.columnconfigure(0, weight=1)
+app.columnconfigure(1, weight=1)
+app.columnconfigure(2, weight=1)
+app.columnconfigure(3, weight=1)
 
 app.rowconfigure(0, weight=0)
 app.rowconfigure(1, weight=0)
@@ -23,25 +22,41 @@ but_cor = '#333333'
 but_hov = '#4D4D4D' 
 text_cor = '#FFFFFF'
 
-resp_frm = ctk.CTkFrame(app, height=150, fg_color='#333333')
+resp_frm = ctk.CTkFrame(app, height=175, fg_color='#333333')
 resp_frm.grid(row=0, column=0, columnspan=4, sticky='new', pady=0, padx=3)
 
-porc_but = ctk.CTkButton(app, text='%', height=50, width=95 , font=('Segoe UI', 24))
-porc_but.grid(row=1, column=0, padx=(3,2), pady=10, sticky='w')
+def but_padrao(nome, texto, linha, coluna):
+    nome = ctk.CTkButton(app, text=texto, height=50, width=95, font=('Segoe UI', 24), fg_color=but_cor, hover_color=but_hov, text_color=text_cor)
+    nome.grid(row=linha, column=coluna, padx=2, pady=2, sticky='new')
 
-ce_but = ctk.CTkButton(app, text='CE', height=50, width=95 , font=('Segoe UI', 24))
-ce_but.grid(row=1, column=1, padx=2, pady=10)
+but_padrao('porc_but', '%', 1, 0)
+but_padrao('ce_but', 'CE', 1, 1)
+but_padrao('c_but', 'C', 1, 2)
+but_padrao('apagar_but', '⌫', 1, 3)
 
-c_but = ctk.CTkButton(app, text='C', height=50, width=95 , font=('Segoe UI', 24))
-c_but.grid(row=1, column=2, padx=2, pady=10)
+but_padrao('1_by_x_but', '1/x', 2, 0)
+but_padrao('quadrado_but', 'x²', 2, 1)
+but_padrao('raiz_but', '²√x', 2, 2)
+but_padrao('div_but', '÷', 2, 3)
 
-apagar_but = ctk.CTkButton(app, text='<', height=50, width=95 , font=('Segoe UI', 24))
-apagar_but.grid(row=1, column=3, padx=(3,2), pady=10, sticky='e')
+but_padrao('7_but', '7', 3, 0)
+but_padrao('8_but', '8', 3, 1)
+but_padrao('9_but', '9', 3, 2)
+but_padrao('mult_but', 'X', 3, 3)
 
+but_padrao('4_but', '4', 4, 0)
+but_padrao('5_but', '5', 4, 1)
+but_padrao('6_but', '6', 4, 2)
+but_padrao('sub_but', '-', 4, 3)
 
+but_padrao('1_but', '1', 5, 0)
+but_padrao('2_but', '2', 5, 1)
+but_padrao('3_but', '3', 5, 2)
+but_padrao('soma_but', '+', 5, 3)
 
-
-
-
+but_padrao('inv_but', '+/-', 6, 0)
+but_padrao('0_but', '0', 6, 1)
+but_padrao('vrg_but', ',', 6, 2)
+but_padrao('result_but', '=', 6, 3)
 
 app.mainloop()
---
+++ 05_09/calculadora_com_for.py
@@ -0,0 +1,47 @@
+import customtkinter as ctk
+from PIL import Image, ImageTk
+
+app = ctk.CTk()
+app.geometry('300x500')
+app.configure(fg_color='#1E1E1E')
+app.title('Calculadora')
+
+app.columnconfigure(0, weight=1)
+app.columnconfigure(3, weight=1)
+
+app.rowconfigure(0, weight=0)
+app.rowconfigure(5, weight=0)
+
+but_cor = '#333333'
+but_hov = '#4D4D4D' 
+text_cor = '#FFFFFF'
+
+resp_frm = ctk.CTkFrame(app, height=175, fg_color='#333333')
+resp_frm.grid(row=0, column=0, columnspan=4, sticky='new', pady=0, padx=3)
+
+#isso é ridiculo wtf
+botoes = [
+    ('%', 'CE', 'C', '⌫'),
+    ('1/x', 'x²', '²√x', '÷'),
+    ('7', '8', '9', 'x'),
+    ('4', '5', '6', '-'),
+    ('1', '2', '3', '+'),
+    ('+/-', '0', ',', '=')
+]
+
+botoes_dic={}
+
+row_index = 0
+for row in botoes:
+    collumn_index = 0
+    for text in row:
+        nome = f'{text}_but'
+        botoes_dic[nome]= ctk.CTkButton(app, text=text, height=50, width=70, font=('Segoe UI', 20), fg_color=but_cor, hover_color=but_hov, text_color=text_cor)
+        botoes_dic[nome].grid(row=row_index + 1, column=collumn_index, padx=2, pady=2, sticky='nsew')
+        collumn_index += 1
+    row_index += 1
+
+for i in (botoes_dic):
+    print(i, '\n')
+
+app.mainloop()
